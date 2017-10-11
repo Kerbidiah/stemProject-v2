3c8824f698a6f32b5e6da30343b4276703618b05
MiningXV <miningxv@gmail.com>
2017-10-11 10:25:15 -0400

    PIN SETUP

--- sketch_oct11b/sketch_oct11b.ino
+++ sketch_oct11b/sketch_oct11b.ino
@@ -1,9 +1,19 @@
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(1,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
