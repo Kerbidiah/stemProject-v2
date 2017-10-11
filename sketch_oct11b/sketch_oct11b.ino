ba7ae6144953c75d221d8f939f5f5f74fc60df0a
MiningXV <miningxv@gmail.com>
2017-10-11 10:44:15 -0400

    minnor fixes/changes
    changed incoming byte to input and declared it

--- sketch_oct11b/sketch_oct11b.ino
+++ sketch_oct11b/sketch_oct11b.ino
@@ -1,3 +1,5 @@
+int input = 0;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(13,OUTPUT);
@@ -19,10 +21,10 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
                 // read the incoming byte:
-                incomingByte = Serial.read();
+                input = Serial.read();
 
                 // say what you got:
                 Serial.print("I received: ");
-                Serial.println(incomingByte, DEC);
+                Serial.println(input, DEC);
         }
 }
