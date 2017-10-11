d77d2907d454a953c7b106486b53ca051ec99d36
MiningXV <miningxv@gmail.com>
2017-10-11 10:39:29 -0400

    want cheerios?????????

--- sketch_oct11b/sketch_oct11b.ino
+++ sketch_oct11b/sketch_oct11b.ino
@@ -11,9 +11,18 @@ void setup() {
   pinMode(3,OUTPUT);
   pinMode(2,OUTPUT);
   pinMode(1,OUTPUT);
+  Serial.begin(9600);
+  Serial.write("test");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  if (Serial.available() > 0) {
+                // read the incoming byte:
+                incomingByte = Serial.read();
+
+                // say what you got:
+                Serial.print("I received: ");
+                Serial.println(incomingByte, DEC);
+        }
 }
