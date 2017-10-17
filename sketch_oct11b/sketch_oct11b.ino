54d76e477762043c9f21c3b524d787433e58676b
MiningXV <miningxv@gmail.com>
2017-10-16 21:16:45 -0400

    if statement #1 and more
    *cleaned up code a bit
    *fixed dececlaration of read func
    *added if #1
    
    (i coulding resit the trump referance)

--- sketch_oct11b/sketch_oct11b.ino
+++ sketch_oct11b/sketch_oct11b.ino
@@ -2,33 +2,40 @@ int input = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13,OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(11,OUTPUT);
-  pinMode(9,OUTPUT);
-  pinMode(8,OUTPUT);
-  pinMode(7,OUTPUT);
-  pinMode(6,OUTPUT);
-  pinMode(5,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(2,OUTPUT);
-  pinMode(1,OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(1, OUTPUT);//magnet
   Serial.begin(9600); //intiates comunication with the computer
   Serial.write("type the lowercase letter corosponding to the answer you deem corect"); // sends stuff to computer
   Serial.write("pardon my speejling")
-  void read() { //defines read func
+}
+
+void read() { //defines read func
     if (Serial.available() > 0) { // gets input from computer
       input = Serial.read(); // _/\_
       return input;
     }
-}
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(1, HIGH)
     Serial.write("Which of these is the best choice to stay safe on social media?");
-    Serial.write("A----Keep a private account.")
-    Serial.write("B----Share your real name with strangers.")
-    Serial.write("C----Sell your soul on eBay.")
+    Serial.write("A----Keep a private account.");
+    Serial.write("B----Share your real name with strangers.");
+    Serial.write("C----Sell your soul on eBay.");
     //write if statement
-  }
+    if(read() == "a"){
+      Serial.write("YOUR RIGHT!!!!!!");
+    } else {
+      Serial.write("FAKE NEWS!!!!!!!");
+      loop();
+    }
 }
