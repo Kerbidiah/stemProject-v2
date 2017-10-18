b6a9e4cf56525728a4b1e25b3eb09b56a1c43555
MiningXV <miningxv@gmail.com>
2017-10-18 10:10:38 -0400

    smushing bugs

--- sketch_oct11b/sketch_oct11b.ino
+++ sketch_oct11b/sketch_oct11b.ino
@@ -15,27 +15,27 @@ void setup() {
   pinMode(1, OUTPUT);//magnet
   Serial.begin(9600); //intiates comunication with the computer
   Serial.write("type the lowercase letter corosponding to the answer you deem corect"); // sends stuff to computer
-  Serial.write("pardon my speejling")
+  Serial.write("pardon my speejling");
 }
 
 void read() { //defines read func
     if (Serial.available() > 0) { // gets input from computer
       input = Serial.read(); // _/\_
-      return input;
+      return(input);
     }
-
+}
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(1, HIGH)
-    Serial.write("Which of these is the best choice to stay safe on social media?");
-    Serial.write("A----Keep a private account.");
-    Serial.write("B----Share your real name with strangers.");
-    Serial.write("C----Sell your soul on eBay.");
-    //write if statement
-    if(read() == "a"){
-      Serial.write("YOUR RIGHT!!!!!!");
-    } else {
-      Serial.write("FAKE NEWS!!!!!!!");
-      loop();
-    }
+  digitalWrite(1, HIGH);
+  Serial.write("Which of these is the best choice to stay safe on social media?");
+  Serial.write("A----Keep a private account.");
+  Serial.write("B----Share your real name with strangers.");
+  Serial.write("C----Sell your soul on eBay.");
+  //write if statement
+  if(read() == "a"){
+    Serial.write("YOUR RIGHT!!!!!!");
+  } else {
+    Serial.write("FAKE NEWS!!!!!!!");
+    loop();
+  }
 }
