aef75ed9c51b890fcfe3ad5e01b3d0577a8dc124
MiningXV <miningxv@gmail.com>
2017-10-26 12:42:00 -0400

    added question 2
    still needs to learn corect answer

--- sketch_oct11b/sketch_oct11b.ino
+++ sketch_oct11b/sketch_oct11b.ino
@@ -54,4 +54,26 @@ void thing() {
     Serial.write("FAKE NEWS!!!!!!!\n");
     thing();
   }
+  //   Q2
+  digitalWrite(1, HIGH);
+  Serial.write("Which of the following is the correct definition of copyright?\n");
+  Serial.write("A----The exclusive legal right, given to an originator or an assignee to print, publish, perform, film, or record literary, artistic, or musical material, and to authorize others to do the same.\n");
+  Serial.write("B----A symbol, word, or words registered to use to represent a company or product.\n");
+  Serial.write("C----A government license conferring a right or title for a set period, especially the sole right to exclude others from making, using, or selling an invention.\n");
+  while(k < 1) {
+    if (Serial.available() > 0) { // gets input from computer
+      answr = Serial.read(); // _/\_
+      k = 1;
+    }
+  }
+  k = 0;
+  Serial.print("I received: ");
+  Serial.println(answr, DEC);
+  Serial.write("\n\n");
+  if(answr == 97) {
+    Serial.write("YOUR RIGHT!!!!!!\n");
+  } else {
+    Serial.write("FAKE NEWS!!!!!!!\n");
+    thing();
+  }
 }
